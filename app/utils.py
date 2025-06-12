14faa8f093c1016d0bb3cbc85bf27cee65cbb0da
Hirusha Adikari <hirushaadi@gmail.com>
2025-06-12 21:56:17 +0530

    add default areas

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,18 @@
 from werkzeug.security import generate_password_hash
-from .models import User
+from .models import User, Area
 from . import db
 
 def create_default_users():
     default_users = [
         {'name': 'Administrator', 'username': 'admin', 'password': '12345678'},
         {'name': 'Hirusha Adikari', 'username': 'hirushaadi', 'password': '12345678'},
+    ]
+    
+    default_areas = [
+        {'name': 'Ambanpola'},
+        {'name': 'Abakolawewa'},
+        {'name': 'Keththaphuwa'},
+        {'name': 'Borawewa'},
     ]
 
     for user in default_users:
@@ -16,5 +23,10 @@ def create_default_users():
                 password=generate_password_hash(user['password'])
             )
             db.session.add(new_user)
+
+    for area in default_areas:
+        if not Area.query.filter_by(name=area['name']).first():
+            new_area = Area(name=area['name'])
+            db.session.add(new_area)
 
     db.session.commit()
