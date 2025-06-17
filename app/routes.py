f1152f8cab0ab50cb765db4593c503a41709d0d7
Hirusha Adikari <hirushaadi@gmail.com>
2025-06-17 21:51:44 +0530

    make sums

--- app/routes.py
+++ app/routes.py
@@ -77,11 +77,15 @@ def add_form():
             if is_eligible_register_info:
                 form.premises_registered = -1
                 form.certificate_displayed = -1
+                form.not_convicted = int(form_data.get('not_convicted'))
+                form.food_not_destroyed = int(form_data.get('food_not_destroyed'))
+                form.sum_general_details = 10 + form.not_convicted + form.food_not_destroyed
             else:    
                 form.premises_registered = int(form_data.get('premises_registered'))
                 form.certificate_displayed = int(form_data.get('certificate_displayed'))
-            form.not_convicted = int(form_data.get('not_convicted'))
-            form.food_not_destroyed = int(form_data.get('food_not_destroyed'))
+                form.not_convicted = int(form_data.get('not_convicted'))
+                form.food_not_destroyed = int(form_data.get('food_not_destroyed'))
+                form.sum_general_details = form.premises_registered + form.certificate_displayed + form.not_convicted + form.food_not_destroyed
 
             # Building
             form.safe_water = int(form_data.get('safe_water'))
@@ -89,6 +93,7 @@ def add_form():
             form.pests_animals = int(form_data.get('pests_animals'))
             form.sound_pollution = int(form_data.get('sound_pollution'))
             form.toilets_cleanliness = int(form_data.get('toilets_cleanliness'))
+            form.sum_building_details = form.safe_water + form.cleanliness + form.pests_animals + form.sound_pollution + form.toilets_cleanliness
             
             # Food Handler
             form.is_eligible_food_handler_info = is_eligible_food_handler_info
@@ -97,11 +102,13 @@ def add_form():
                 form.proper_clothing = -1
                 form.unhygienic_behaviour = -1
                 form.clean_utensils = -1
+                form.sum_food_handler = 20
             else:
                 form.medical_certificates = int(form_data.get('medical_certificates'))
                 form.proper_clothing = int(form_data.get('proper_clothing'))
                 form.unhygienic_behaviour = int(form_data.get('unhygienic_behaviour'))
                 form.clean_utensils = int(form_data.get('clean_utensils'))
+                form.sum_food_handler = form.medical_certificates + form.proper_clothing + form.unhygienic_behaviour + form.clean_utensils
             
             # Processing and Serving
             form.is_eligible_processing_info = is_eligible_processing_info
@@ -112,6 +119,7 @@ def add_form():
                 form.food_surfaces_clean = -1
                 form.wastewater_disposal = -1
                 form.closed_bins = -1
+                form.sum_processing_and_serving = 20
             else:
                 form.walls_hygienic = int(form_data.get('walls_hygienic'))
                 form.floor_hygienic = int(form_data.get('floor_hygienic'))
@@ -119,6 +127,7 @@ def add_form():
                 form.food_surfaces_clean = int(form_data.get('food_surfaces_clean'))
                 form.wastewater_disposal = int(form_data.get('wastewater_disposal'))
                 form.closed_bins = int(form_data.get('closed_bins'))
+                form.sum_processing_and_serving = form.walls_hygienic + form.floor_hygienic + form.ceiling_hygienic + form.food_surfaces_clean + form.wastewater_disposal + form.closed_bins
                 
             # Food Storage
             form.is_eligible_food_storage_info = is_eligible_food_storage_info
@@ -127,14 +136,17 @@ def add_form():
                 form.cooked_food_temp = -1
                 form.cooked_food_container = -1
                 form.cooked_food_contam_prevented = -1
+                form.uncooked_food_contam_prevented = int(form_data.get('uncooked_food_contam_prevented'))
+                form.sum_food_storage = 16 + form.uncooked_food_contam_prevented
             else:
                 form.cooked_food_closed = int(form_data.get('cooked_food_closed'))
                 form.cooked_food_temp = int(form_data.get('cooked_food_temp'))
                 form.cooked_food_container = int(form_data.get('cooked_food_container'))
                 form.cooked_food_contam_prevented = int(form_data.get('cooked_food_contam_prevented'))
+                form.uncooked_food_contam_prevented = int(form_data.get('uncooked_food_contam_prevented'))
+                form.sum_food_storage = form.cooked_food_closed + form.cooked_food_temp + form.cooked_food_container + form.cooked_food_contam_prevented + form.uncooked_food_contam_prevented
             
-            # This is always required and must be submitted normally
-            form.uncooked_food_contam_prevented = int(form_data.get('uncooked_food_contam_prevented'))
+            form.sum_all = form.sum_general_details + form.sum_building_details + form.sum_food_handler + form.sum_processing_and_serving + form.sum_food_storage  
             
             db.session.add(form)
             db.session.commit()
